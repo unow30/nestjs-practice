2e63de2acef58e1d23684b9e49e75fdbbbf1ec97
unow30 <yunho7205@gmail.com>
2025-01-09 20:30:52 +0900

    commit

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
