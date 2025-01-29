abf853d6a64c5894f17a69f3c5db8ea769ac801a
unow30 <yunho7205@gmail.com>
2025-01-29 16:22:36 +0900

    mapped types test

--- src/genre/dto/update-genre.dto.ts
+++ src/genre/dto/update-genre.dto.ts
@@ -1,10 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateGenreDto } from './create-genre.dto';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
-export class UpdateGenreDto {
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  name?: string;
-}
+export class UpdateGenreDto extends PartialType(CreateGenreDto) {}
