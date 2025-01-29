5d92f876f4c4c2d54cf42337b7871a39afd6817d
unow30 <yunho7205@gmail.com>
2025-01-29 16:20:50 +0900

    mapped types test

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,39 +1,4 @@
-import {
-  ArrayNotEmpty,
-  IsArray,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMovieDto } from './create-movie.dto';
 
-enum MovieGenre {
-  Fantasy = 'fantasy',
-  Action = 'action',
-}
-
-export class UpdateMovieDto {
-  /*
-   * @IsNotEmpty Checks if given value is not empty (!== '', !== null, !== undefined).
-   * @IsOptional Checks if value is missing and if so, ignores all validators.
-   * */
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  title?: string;
-
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsNumber({}, { each: true })
-  @IsOptional()
-  genreIds?: number[];
-
-  @IsNotEmpty()
-  @IsOptional()
-  detail?: string;
-
-  @IsNotEmpty()
-  @IsNumber()
-  @IsOptional()
-  directorId?: number;
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
