d25e65a317ed3c53e549173acf025226f3519cc7
unow30 <yunho7205@gmail.com>
2025-02-27 13:18:36 +0900

    add swagger apiOperation documents

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -5,25 +5,46 @@ import {
   IsNumber,
   IsString,
 } from 'class-validator';
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMovieDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 제목',
+  })
   title: string;
 
   @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: '영화 제목 상세',
+    example: '제목 상세',
+  })
   detail: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    description: '감독 id',
+    example: 1,
+  })
   directorId: number;
 
   @IsArray()
   @ArrayNotEmpty()
   @IsNumber({}, { each: true })
   @Type(() => Number)
+  @ApiProperty({
+    description: '장르 ids',
+    example: [1],
+  })
+  @Transform(({ value }) => (Array.isArray(value) ? value : [value]))
   genreIds: number[];
 
   @IsString()
+  @ApiProperty({
+    description: '영화 파일 이름',
+  })
   movieFileName: string;
 }
