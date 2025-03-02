6d320df0b070ad08ecade2d78e45b0af68a33f12
unow30 <yunho7205@gmail.com>
2025-03-02 15:19:35 +0900

    socket io test chat, chatroom entity test

---
+++ src/chat/entity/chat-room.entity.ts
@@ -0,0 +1,23 @@
+import {
+  BaseEntity,
+  Column,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Chat } from './chat.entity';
+import { User } from '../../user/entity/user.entity';
+
+export class ChatRoom extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToMany(() => User, (user) => user.chatRooms)
+  @JoinTable()
+  users: User[];
+
+  @OneToMany(() => Chat, (chat) => chat.chatRoom)
+  chats: Chat[];
+}
---
+++ src/chat/entity/chat.entity.ts
@@ -0,0 +1,18 @@
+import { User } from 'src/user/entity/user.entity';
+import { BaseTable } from '../../common/entity/base-table.entity';
+import { Column, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { ChatRoom } from './chat-room.entity';
+
+export class Chat extends BaseTable {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.chats)
+  author: User;
+
+  @Column()
+  message: string;
+
+  @ManyToOne(() => ChatRoom, (chatroom) => chatroom.chats)
+  chatRoom: ChatRoom;
+}
