62a428c04db0748af1393ca281d627f5e0044445
unow30 <yunho7205@gmail.com>
2025-03-24 23:20:30 +0900

    test

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,11 +1,6 @@
-import {
-  BadRequestException,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { SelectQueryBuilder } from 'typeorm';
 import { PagePaginationDto } from './dto/page-pagination.dto';
-import { CursorPaginationDto } from './dto/cursor-pagination.dto';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { ObjectCannedACL, PutObjectCommand, S3 } from '@aws-sdk/client-s3';
 import { v4 as Uuid } from 'uuid';
@@ -82,97 +77,5 @@ export class CommonService {
     const skip = (page - 1) * take;
     qb.take(take);
     qb.skip(skip);
-  }
-
-  async applyCursorPaginationParamsToQb<T>(
-    qb: SelectQueryBuilder<T>,
-    dto: CursorPaginationDto,
-  ) {
-    const { cursor, take } = dto;
-    let { order } = dto;
-
-    if (cursor) {
-      const decodedCursor = Buffer.from(cursor, 'base64').toString('utf-8');
-      /**
-       * cursor 구조
-       * {
-       *   values: {
-       *     id: 27
-       *   },
-       *   order:['id_desc']
-       * }
-       * */
-      const cursorObj = JSON.parse(decodedCursor);
-
-      order = cursorObj.order;
-
-      const { values } = cursorObj;
-
-      const columns = Object.keys(values);
-      // > : <
-      const comparisonOperator = order.some((o) => o.endsWith('DESC'))
-        ? '<'
-        : '>';
-
-      //(column1, column2, column3)
-      const whereConditions = columns.map((c) => `${qb.alias}.${c}`).join(',');
-
-      //(:value1, :value2, :value3)
-      const whereParams = columns.map((c) => `:${c}`).join(',');
-
-      qb.where(
-        // (column1, column2, column3) > (:value1, :value2, :value3)
-        `(${whereConditions}) ${comparisonOperator} (${whereParams})`,
-        values,
-      );
-    }
-
-    // order = [likeCount_desc, id_desc]
-    for (let i = 0; i < order.length; i++) {
-      const [column, direction] = order[i].split('_');
-
-      if (direction !== 'ASC' && direction !== 'DESC') {
-        throw new BadRequestException('order는 ASC 또는 DESC로 던저주세요');
-      }
-
-      if (i === 0) {
-        qb.orderBy(`${qb.alias}.${column}`, direction);
-      } else {
-        qb.addOrderBy(`${qb.alias}.${column}`, direction);
-      }
-    }
-    qb.take(take);
-
-    const result = await qb.getMany();
-    const nextCursor = this.generateNextCursor(result, order);
-    return { qb, nextCursor };
-  }
-
-  //<T>를 받는 이유: 쿼리 실행 응답값을 넣어주기 위해서. 서버에서 cursor를 만들어 주기 위해서 필요
-  generateNextCursor<T>(results: T[], order: string[]): string | null {
-    if (results.length === 0) return null; //응답값이 없다면 마지막 데이터다.
-    /**
-     * cursor 구조
-     * {
-     *   values: {
-     *     id: 27
-     *   },
-     *   order:['id_desc']
-     * }
-     * */
-    const lastItem = results[results.length - 1];
-    const values = {};
-
-    order.forEach((columnOrder) => {
-      const [column] = columnOrder.split('_');
-      values[column] = lastItem[column];
-    });
-
-    const cursorObj = { values, order };
-    const nextCursor = Buffer.from(JSON.stringify(cursorObj)).toString(
-      'base64',
-    );
-    // console.log('nextCursor', nextCursor);
-    return nextCursor;
   }
 }
